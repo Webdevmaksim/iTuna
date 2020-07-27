1aa97e37021dbde847ca8f3d0313d0456b38f4a5
Webdevmaksim <maksim.saraev.121.5@gmail.com>
2020-07-27 11:50:57 +0300

    Feat: import + index + active class toggle

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+import { radioPlayerInit } from './radioPlayer.js';
+import { musicPlayerInit } from './musicPlayer.js';
+import { videoPlayerInit } from './videoPlayer.js';
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+// Deactivation of active classes
+const deactivationPlayer = () => {
+    temp.style.display = 'none';
+    playerBtn.forEach(item => item.classList.remove('active'));
+    playerBlock.forEach(item => item.classList.remove('active'));
+};
+
+//Finding index of elements and adding an active class
+playerBtn.forEach((btn, i) => btn.addEventListener('click', () => {
+        deactivationPlayer();
+        btn.classList.add('active');
+        playerBlock[i].classList.add('active');
+    }));
+
+radioPlayerInit();
+musicPlayerInit();
+videoPlayerInit();
